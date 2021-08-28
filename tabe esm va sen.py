c9db5d5e6fcc73124774afed4087aeb38d0a232d
arshiami <88331022+arshiami@users.noreply.github.com>
2021-08-28 15:15:51 +0430

    Create tabe esm va sen.py

---
+++ tabe esm va sen.py
@@ -0,0 +1,10 @@
+def printer(s1, s2, a1, a2):
+    print("The difference of ages is" ,s1-s2)
+
+while True:    
+    name1 = input("please enter your name:")
+    age1 = int(input())
+    name2 = input("What is the second name?")
+    age2 = int(input())
+    printer(age1,age2,name1,name2)
+    
